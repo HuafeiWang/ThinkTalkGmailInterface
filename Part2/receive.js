3cfc8f3b8013a66dec98a1ee8bce77812a1f9140
Huafei Wang <huafeiwang@DN51vdls.SUNet>
2016-03-02 15:41:56 -0800

    Receive.js updated

--- Part2/receive.js
+++ Part2/receive.js
@@ -1,20 +1,42 @@
 const Tp = require('thingpedia');
 
 module.exports = new Tp.ChannelClass({
-    Name: 'TwitterSinkChannel',
+    Name: 'GmailPollingTrigger',
+    Extends: Tp.PollingTrigger
+    RequiredCapabilities: ['channel-state']
 
-    _init: function(engine, device) {
+    _init: function(engine, state, device) {
         this.parent();
-
-        this._twitter = device.queryInterface('twitter');
+        this.interval = 3600000;
+        this.state = state;
+        this.url = 'https://www.googleapis.com/gmail/v1/users/'+this.state.userID+'/messages?maxResults=1';
+        this.auth = 'Bearer ' + device.accessToken;
+        // this.maxResults = 1;
     },
 
-    sendEvent: function(event) {
-        console.log('Posting Twitter event', event);
+    _onResponse: function(data) {
+        // do something
+        var thread_IDs = new Array();
+        var package = JSON.parse(JSON.stringify(data));
+        var msgs = package.messages;
+        var newest_msg = msgs[0];
+        var threadId = newest_msg.threadId;
+        // for (var i=0; i<msgs.length; i++){
+        //     var msg = package.messages[i];
+        //     // thread_IDs.push(msg.threadId);
+        // }
 
-        var status = event[0];
-        this._twitter.postTweet({ status: status }, function(err) {
-            console.log('Tweeting failed: ' + err);
-        }, function() { });
+        var auth = 'Bearer ' + device.accessToken;
+        Tp.Helpers.Http.get('www.googleapis.com/oauth2/v2/userinfo', {auth: auth, accept: 'application/json'})
+            .then(function(response) {
+                var parsed = JSON.parse(response);
+                console.log('parsed', parsed);
+                // TODO: Extract JSON information and emit to the user
+                var from = ;
+                var content = ;
+                var messageID = ;
+                emitEvent([from, content, messageID]);
+        });
+
     },
 });
--- Part2/trash.js
+++ Part2/trash.js
@@ -5,6 +5,7 @@ module.exports = new Tp.ChannelClass({
 
     _init: function(engine, device) {
         this.parent();
+        this.device = device;
 
         // this.url = 'https://www.googleapis.com/gmail/v1/users/'+this.state.userID+'/messages?maxResults=1';
         // this.auth = 'Bearer ' + device.accessToken;
@@ -13,7 +14,7 @@ module.exports = new Tp.ChannelClass({
     sendEvent: function(event) {
         var messageID = event[0];
         console.log('Trashing MessageID: ' + messageID);
-        var auth = 'Bearer ' + device.accessToken;
-        Tp.Helpers.Http.get('https://www.googleapis.com/gmail/v1/me/userId/messages/' + messageID + '/trash', {auth: auth, accept: 'application/json'});
+        var auth = 'Bearer ' + this.device.accessToken;
+        Tp.Helpers.Http.post('https://www.googleapis.com/gmail/v1/me/userId/messages/' + messageID + '/trash', {auth: auth, accept: 'application/json'});
     }
 });
