8a1d42704c708ee4b31fce7ff249be2bc9e3b74e
snail <yy0222@stanford.edu>
2016-03-02 13:59:15 -0800

    receiving: working on the 2nd get func

--- Part2/receive.js
+++ Part2/receive.js
@@ -1,20 +1,53 @@
 const Tp = require('thingpedia');
 
 module.exports = new Tp.ChannelClass({
-    Name: 'TwitterSinkChannel',
+    Name: 'GmailPollingTrigger',
+    Extends: Tp.PollingTrigger
+    RequiredCapabilities: ['channel-state']
 
-    _init: function(engine, device) {
+    _init: function(engine, state, device) {
         this.parent();
-
-        this._twitter = device.queryInterface('twitter');
+        this.interval = 3600000;
+        this.state = state;
+        this.url = 'https://www.googleapis.com/gmail/v1/users/'+this.state.userID+'/messages?maxResults=1';
+        this.auth = 'Bearer ' + device.accessToken;
+        // this.maxResults = 1;
     },
 
-    sendEvent: function(event) {
-        console.log('Posting Twitter event', event);
-
-        var status = event[0];
-        this._twitter.postTweet({ status: status }, function(err) {
-            console.log('Tweeting failed: ' + err);
-        }, function() { });
+    _onResponse: function(data) {
+        // do something
+        var thread_IDs = new Array();
+        var package = JSON.parse(JSON.stringify(data));
+        var msgs = package.messages;
+        var newest_msg = msgs[0];
+        var threadId = newest_msg.threadId;
+        // for (var i=0; i<msgs.length; i++){
+        //     var msg = package.messages[i];
+        //     // thread_IDs.push(msg.threadId);
+        // }
     },
 });
+
+
+
+// const Tp = require('thingpedia');
+
+// module.exports = new Tp.ChannelClass({
+//     Name: 'TwitterSinkChannel',
+
+//     _init: function(engine, device) {
+//         this.parent();
+
+//         this._twitter = device.queryInterface('twitter');
+//     },
+
+//     sendEvent: function(event) {
+//         console.log('Posting Twitter event', event);
+
+//         var status = event[0];
+//         this._twitter.postTweet({ status: status }, function(err) {
+//             console.log('Tweeting failed: ' + err);
+//         }, function() { });
+//     },
+// });
+
