e1531fce80ee11f7847d262643655990a2eb704c
snail <yy0222@stanford.edu>
2016-03-02 16:03:44 -0800

    receive update

--- Part2/receive.js
+++ Part2/receive.js
@@ -9,33 +9,50 @@ module.exports = new Tp.ChannelClass({
         this.parent();
         this.interval = 3600000;
         this.state = state;
-        this.url = 'https://www.googleapis.com/gmail/v1/users/'+this.state.userID+'/messages?maxResults=1';
+        this.device = device;
+        this.url = 'https://www.googleapis.com/gmail/v1/users/'+this.device.userID+'/messages?maxResults=1';
         this.auth = 'Bearer ' + device.accessToken;
     },
 
     _onResponse: function(data) {
         // do something
-        var thread_IDs = new Array();
-        var package = JSON.parse(JSON.stringify(data));
+        // var thread_IDs = new Array();
+        var package = JSON.parse(/*JSON.stringify*/(data));
         var msgs = package.messages;
         var newest_msg = msgs[0];
         var threadId = newest_msg.threadId;
+        
 
-        if (state.get(threadId) == null) {
-            state.set(threadId, "1");
+        if (this.state.get(threadId) === null){
+            this.state.set(threadId, "1");
 
-            var auth = 'Bearer ' + device.accessToken;
-            Tp.Helpers.Http.get('https://www.googleapis.com/gmail/v1/users/' + this.device.userId + '/messages/' + threadId, {auth: auth, accept: 'application/json'})
+            var getUrl = 'https://www.googleapis.com/gmail/v1/users/'+this.device.userID+'/messages/'+threadId;
+            Tp.Helpers.Http.get(getUrl, {auth: this.auth, accept: 'application/json'})
                 .then(function(response) {
                     var parsed = JSON.parse(response);
                     console.log('parsed', parsed);
-                    // TODO: Extract JSON information and emit to the user
-                    var from = ;
-                    var content = ;
-                    var messageID = ;
-                    emitEvent([from, content, messageID]);
-            });
+                    var headers = parsed.payload.headers;
+                    var sender, subject;
+                    for (var i=0; i<headers.length; i++){
+                        var header = headers[i];
+                        if (header.name === 'From'){
+                            sender = header.value;
+                        }else if(header.name === 'Subject'){
+                            subject = header.value;
+                        }
+                    }
 
-        }
+                    eventEmit([sender, subject, threadId]);
+
+
+                    
+                });
+            }
+
+
+        
     },
 });
+
+
+
